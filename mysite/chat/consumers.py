2a747c4b808caad9bbf5ce07f71261006b37ee10
Peter Lesslie <pclesslie@gmail.com>
2019-06-25 19:21:05 -0500

    Basic websocket functionality

---
+++ mysite/chat/consumers.py
@@ -0,0 +1,20 @@
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+
+# TODO: convert to async consumer
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        self.send(text_data=json.dumps({
+            'message': message,
+        }))
+
