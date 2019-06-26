a0c4d21880488bbcd0d5f4fed3189b136b1c0ddd
Peter Lesslie <pclesslie@gmail.com>
2019-06-25 19:48:32 -0500

    Working synchronous chat server

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ASGI_APPLICATION = 'mysite.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            # TODO: figure out how to configure this for heroku
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
