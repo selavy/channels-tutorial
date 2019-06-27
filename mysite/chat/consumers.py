bb21e5c6e471a3c10532263bb6c3de7bd9720abd
Peter Lesslie <pclesslie@gmail.com>
2019-06-27 10:23:48 -0500

    Example getting logged in user

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -1,5 +1,6 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
+import channels.auth
 import json
 
 
@@ -40,8 +41,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event['message']
 
+        # # XXX: this is an example of how to get the current user:
+        # # if the user hasn't been loaded yet, then it will return an instance
+        # # of AnonymousUser():
+        # user = self.scope.get('user', 'UNKNOWN')
+        # user = await channels.auth.get_user(self.scope)
+
         # send on websocket to UI
         await self.send(text_data=json.dumps({
             'message': message,
-            # 'user': scope['user'],
         }))
